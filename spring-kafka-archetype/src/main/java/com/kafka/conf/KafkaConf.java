7582ba01988abeae8c21f731aa73d7c87aeabcc9
Gmy <guomuye1991@gmail.com>
2018-05-28 12:20:56 +0800

    spring-kafka

---
+++ spring-kafka-archetype/src/main/java/com/kafka/conf/KafkaConf.java
@@ -0,0 +1,12 @@
+package com.kafka.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
+public class KafkaConf {
+
+
+
+}
